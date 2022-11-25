b2113de4a5383466946e8686b96658e59f65d3b7
hunarjain09 <huja4688@colorado.edu>
2022-11-25 09:39:28 +0530

    nats working example

--- deploy-local-dev.sh
+++ deploy-local-dev.sh
@@ -18,8 +18,10 @@ kubectl apply -f worker/worker-deployment.yaml
 kubectl apply -f minio/minio-external-service.yaml
 
 
-kubectl port-forward --address 0.0.0.0 service/redis 6379:6379 &
+# kubectl port-forward --address 0.0.0.0 service/redis 6379:6379 &
 
 # If you're using minio from the kubernetes tutorial this will forward those
 kubectl port-forward -n minio-ns --address 0.0.0.0 service/minio-proj 9000:9000 &
 kubectl port-forward -n minio-ns --address 0.0.0.0 service/minio-proj 9001:9001 &
+
+kubectl port-forward --address 0.0.0.0 service/my-nats 4222:4222 &
