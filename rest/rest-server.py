91aac668b16e44e585a190a91d62cb71244a0876
Varad Raut <varadraut@gmail.com>
2022-12-08 09:41:28 -0700

    Made NATS client global. Initializing the client only when needed, instead of creating and connecting for everyrequest

--- rest/rest-server.py
+++ rest/rest-server.py
@@ -3,7 +3,7 @@ import hashlib
 import io
 import os
 import asyncio
-import nats
+from nats.aio.client import Client as NATS
 from flask import Flask, request, send_file
 from minio import Minio
 
@@ -30,13 +30,21 @@ if not minio_client.bucket_exists(minio_input_bucket):
 if not minio_client.bucket_exists(minio_output_bucket):
     minio_client.make_bucket(minio_output_bucket)
 
+nats_client = NATS()
+
+async def init_nats_client():
+    if not nats_client.is_connected:
+        await nats_client.connect(nats_host)
+        print(f'Connected to NATS at {nats_client.connected_url.netloc}')
+
 @app.route('/', methods=['GET'])
-def root():
+async def root():
     return 'Use the /apiv1/ API'
 
 # Defining /apiv1/trim API
 @app.route('/apiv1/trim', methods=['POST'])
 async def trim():
+    await init_nats_client()
     # Handling request data
     data: dict = request.json or {}
     mp4 = base64.b64decode(data.get('mp4', ''))
@@ -45,11 +53,10 @@ async def trim():
 
     minio_client.put_object(minio_input_bucket, video_hash, io.BytesIO(mp4), len(mp4))
     
-    nc = await nats.connect(nats_host) # TODO: Move NATS connection to global?
     message = video_hash
-    await nc.publish(nats_subject, message.encode())
-    await nc.close()
+    await nats_client.publish(nats_subject, message.encode())
 
     return { 'hash': video_hash }
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
